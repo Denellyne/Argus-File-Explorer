a1ba856bf55000127deb8b093d18ee7be9e85351
Gustavo dos Santos <gustavobarbosavs@gmail.com>
2023-11-15 20:29:28 +0000

    Fixed GitIgnore

---
+++ customInclude/Functionality/Debug/debug.cpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "debug.h"
+#include "Functionality/Search/Search.h"
+
+void Debug::print(std::vector<Directory>& directories, std::vector<File>& files)
+{
+    std::cout << "Directories:\n\n";
+    for (int i = 0; i < directories.size(); i++)
+        std::cout << directories[i].filePath << '\n';
+    std::cout << "\nFiles:\n\n";
+    for (int i = 0; i < files.size(); i++)
+        std::cout << files[i].filePath.substr(files[i].filePath.find_last_of("//") + 1) << "\nSize: " << files[i].fileSizeKbs << " Kbs " << "\nLast modified time: " << files[i].lastModificationTime << '\n' << '\n';
+}
+
---
+++ customInclude/Functionality/Debug/debug.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Functionality/Search/Search.h"
+
+namespace Debug {
+
+	void print(std::vector<Directory>& directories, std::vector<File>& files);
+
+}
+
