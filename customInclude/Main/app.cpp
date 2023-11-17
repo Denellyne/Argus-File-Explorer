aa8eb17941c6f6c05bef231133c3532346be68a9
Denellyne <gustavobarbosavs@gmail.com>
2023-11-17 00:17:55 +0000

    Update

--- customInclude/Main/app.cpp
+++ customInclude/Main/app.cpp
@@ -9,10 +9,38 @@
 #include "imgui_impl_opengl3.h"
 #include "imgui_stdlib.h"
 #include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#define GL_CLAMP_TO_EDGE 0x812F
+#include "stb_image.h"
 
-void app(std::vector<Directory>& directories, std::vector<File>& files, std::stack<std::string>& directoryStack);
+bool LoadTextureFromFile(const char* filename, GLuint* out_texture);
+int GUI();
 
-int GUI(){
+void app(std::vector<Directory>& directories, std::vector<File>& files, std::stack<std::string>& directoryStack) {
+    using namespace ImGui;
+
+    std::string userInputDirectory;
+    ImGuiIO& io = ImGui::GetIO();
+    PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+    SetNextWindowSize(ImVec2(io.DisplaySize.x,io.DisplaySize.y));
+    SetNextWindowPos(ImVec2(0,0));
+
+    Begin("##FileExplorer",NULL,ImGuiWindowFlags_NoTitleBar);
+    PopStyleVar();
+    
+    //Search Bar
+    if (InputText("Directory", &userInputDirectory, ImGuiInputTextFlags_EnterReturnsTrue)) searchNewPath(userInputDirectory, directories, files, directoryStack);
+
+    //Display icons
+    //for (int i = 0;i < files.size();i++) Image((void*)(intptr_t)my_image_texture, ImVec2(my_image_width/4, my_image_height/4));
+
+    BeginChild("Files", { 0,0 }, false, ImGuiWindowFlags_AlwaysVerticalScrollbar);
+    EndChild();
+
+    End();
+}
+
+int GUI() {
     std::vector<Directory> directories(0, Directory("C:\\"));
     std::vector<File> files(0, File("fileExplorer.exe"));
     std::stack<std::string> directoryStack;
@@ -20,23 +48,29 @@ int GUI(){
     directoryStack.push("C:\\");
 
     glfwInit();
-    
+
     GLFWwindow* window = glfwCreateWindow(1280, 720, "File Explorer", nullptr, nullptr);
     if (window == nullptr) return 1;
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
+    GLuint folderIcon = 0;
+    GLuint fileIcon = 0;
+    LoadTextureFromFile(".icons/folder.jpg", &folderIcon);
+    //LoadTextureFromFile(".icons/file.jpg", &fileIcon);
+
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO();
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     
-    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;    
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
     ImGui::StyleColorsDark();
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330");
-    driveIndex(drive);
+
+    //driveIndex(drive);
 
     while (!glfwWindowShouldClose(window)) { // Render
         glfwPollEvents();
@@ -44,8 +78,7 @@ int GUI(){
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
-       
-        app(directories,files,directoryStack);
+        app(directories, files, directoryStack);
 
         ImGui::Render();
 
@@ -64,49 +97,37 @@ int GUI(){
     return 0;
 }
 
-void app(std::vector<Directory>& directories, std::vector<File>& files, std::stack<std::string>& directoryStack) {
-    using namespace ImGui;
+bool LoadTextureFromFile(const char* filename, GLuint* out_texture)
+{
+    // Load from file
+    int image_width = 0;
+    int image_height = 0;
+    unsigned char* image_data = stbi_load(filename, &image_width, &image_height, NULL, 4);
+    if (image_data == NULL)
+        return false;
 
-    std::string userInputDirectory;
-    ImGuiIO& io = ImGui::GetIO();
-    PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
-    SetNextWindowSize(ImVec2(io.DisplaySize.x,io.DisplaySize.y));
-    SetNextWindowPos(ImVec2(0,0));
+    // Create a OpenGL texture identifier
+    GLuint image_texture;
+    glGenTextures(1, &image_texture);
+    glBindTexture(GL_TEXTURE_2D, image_texture);
 
-    Begin("##FileExplorer",NULL,ImGuiWindowFlags_NoTitleBar);
-    PopStyleVar();
+    // Setup filtering parameters for display
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // This is required on WebGL for non power-of-two textures
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Same
 
-    if (InputText("Directory", &userInputDirectory, ImGuiInputTextFlags_EnterReturnsTrue)) searchNewPath(userInputDirectory, directories, files, directoryStack);
-    BeginChild("Files", { 0,0 }, false, ImGuiWindowFlags_AlwaysVerticalScrollbar);
-    EndChild();
+    // Upload pixels into texture
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
+    stbi_image_free(image_data);
 
+    *out_texture = image_texture;
 
-    End();
+    return true;
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Ignore
 
 //void fileExplorer(std::vector<Directory>& directories, std::vector<File>& files, std::stack<std::string>& directoryStack) {
 //    int choice = 0;
