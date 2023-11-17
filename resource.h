9b30ce00f096cf2e2ab14717f255315e596fc0a6
Gustavo dos Santos <gustavobarbosavs@gmail.com>
2023-11-17 12:22:08 +0000

    Fixed

---
+++ resource.h
@@ -0,0 +1,13 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by fileExplorer1.rc
+// Pr�ximos valores padr�es para novos objetos
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
